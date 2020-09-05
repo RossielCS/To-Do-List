3f9abde08c3038fd6b3c9d0060d484cee2d92c0c
Rossiel Carranza <60085697+RossielCS@users.noreply.github.com>
2020-09-04 23:22:15 -0300

    Add icons for navbar
    - Add file loader

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import './assets/stylesheets/style.scss';
 import { creator } from './modules/aux-methods';
 import { Project, startProject, projectsCont } from './modules/project';
 import { ToDo, toDosCont, getToDosStorage } from './modules/todo';
-import { createNav, liNamesList } from './modules/dom-navbar';
+import {
+  createNav, liIds, liNames, liIcons,
+} from './modules/dom-navbar';
 import createProjectModal from './modules/dom-modal-proj';
 import { createToDoModal, inputInfo, radioButtons } from './modules/dom-modal-todo';
 import createShowToDos from './modules/dom-show-todos';
@@ -13,7 +15,7 @@ import createShowProj from './modules/dom-show-proj';
 const content = document.getElementById('content');
 
 const main = creator(content, 'main', 'append');
-createNav(main, liNamesList);
+createNav(main, liIds, liNames, liIcons);
 
 const displaySection = creator(main, 'article', 'append');
 displaySection.setAttribute('id', 'sect-selected-todo');
@@ -24,17 +26,13 @@ getToDosStorage(toDosCont);
 const navList = document.getElementsByTagName('ul')[0].children;
 
 navList[0].addEventListener('click', () => {
-  window.localStorage.clear();
-});
-
-navList[1].addEventListener('click', () => {
   if (!document.querySelector('.modal')) {
     const modalProject = createProjectModal(main, projectsCont, Project);
     modalProject.style.display = 'block';
   }
 });
 
-navList[2].addEventListener('click', () => {
+navList[1].addEventListener('click', () => {
   if (!document.querySelector('.modal')) {
     const modalToDo = createToDoModal(
       main, inputInfo, radioButtons, projectsCont, ToDo, projectsCont,
@@ -43,12 +41,12 @@ navList[2].addEventListener('click', () => {
   }
 });
 
-navList[3].addEventListener('click', () => {
+navList[2].addEventListener('click', () => {
   createShowProj(displaySection, projectsCont);
 });
 
-navList[4].addEventListener('click', () => {
+navList[3].addEventListener('click', () => {
   createShowToDos(displaySection, projectsCont);
 });
 
-navList[4].click();
+navList[3].click();
--- src/modules/dom-navbar.js
+++ src/modules/dom-navbar.js
@@ -1,19 +1,42 @@
 import { creator } from './aux-methods';
+import * as images from '../assets/images';
 
-function createNav(main, liNamesList) {
+const liNames = [
+  'Add a Project', ' Add a To-Do', 'All Projects', 'All To-Dos',
+];
+
+const liIds = [
+  'add-proj', 'add-todo', 'all-proj', 'all-todos',
+];
+
+const liIcons = [
+  images.add_proj, images.add_todo, images.all_proj, images.all_todos,
+];
+
+/*
+function createBg(menuCell, index, saladsImages) {
+  menuCell.setAttribute('class', 'menu-salad-bg');
+  menuCell.style.backgroundImage = `url('${saladsImages}')`;
+}
+*/
+
+function createNav(main, liIds, liNames, liIcons) {
   const navbar = creator(main, 'nav', 'append');
   const navList = creator(navbar, 'ul', 'append');
-  for (let i = 0; i < 5; i += 1) {
+  for (let i = 0; i < 4; i += 1) {
     const li = creator(navList, 'li', 'append');
-    li.setAttribute('id', `${liNamesList[i]}`);
-    li.innerHTML = `${liNamesList[i]}`;
+    li.setAttribute('id', `${liIds[i]}`);
+
+    const icon = creator(li, 'div', 'append');
+    icon.style.backgroundImage = `url('${liIcons[i]}')`;
+
+    const text = creator(li, 'p', 'append');
+    text.innerHTML = `${liNames[i]}`;
   }
 
   return navbar;
 }
 
-const liNamesList = [
-  'search-bar', 'add-proj', 'add-todo', 'all-proj', 'all-todos',
-];
-
-export { createNav, liNamesList };
+export {
+  createNav, liIds, liNames, liIcons,
+};
